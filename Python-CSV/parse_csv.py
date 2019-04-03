21c417d79378519bfd63172ecfe690dfa3dc37f5
raghavo9 <44409531+raghavo9@users.noreply.github.com>
2019-04-03 20:38:09 +0530

    Update parse_csv.py

--- Python-CSV/parse_csv.py
+++ Python-CSV/parse_csv.py
@@ -1,5 +1,57 @@
 import csv
 
+
+import csv
+with open("new_names.csv",'r') as csvf:
+    read_csv=csv.reader(csvf)
+    next(read_csv)
+    for x in read_csv:
+        print(x[2])
+ '''
+ OUTPUT:>>>
+ john-doe@bogusemail.com
+maryjacobs@bogusemail.com
+davesmith@bogusemail.com
+janestuart@bogusemail.com
+tomwright@bogusemail.com'
+ 
+ '''
+   
+    
+with open("new_names.csv",'r') as csvf:
+    read_csv=csv.reader(csvf)
+    with open("my_newcsvfile.csv",'w') as ncsvf:
+        write_csv=csv.writer(ncsvf,delimiter='-')
+        for x in read_csv:
+            write_csv.writerow(x)
+
+            
+   #then new file will bw as follows            
+'''
+
+first_name-last_name-email
+
+John-Doe-"john-doe@bogusemail.com"
+
+Mary-"Smith-Robinson"-maryjacobs@bogusemail.com
+
+Dave-Smith-davesmith@bogusemail.com
+
+Jane-Stuart-janestuart@bogusemail.com
+
+Tom-Wright-tomwright@bogusemail.com
+
+Steve-Robinson-steverobinson@bogusemail.com
+
+Nicole-Jacobs-nicolejacobs@bogusemail.com
+
+'''
+
+
+
+
+        
+
 with open('names.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
