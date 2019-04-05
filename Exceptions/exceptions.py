72e7bf358b7f2a123a66ec8d466a133f7d4a998c
raghavo9 <44409531+raghavo9@users.noreply.github.com>
2019-04-05 21:11:29 +0530

    Update exceptions.py

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -1,3 +1,29 @@
+try:
+    with open("test.txt",'r') as r:
+        s=s
+        pass
+except FileNotFoundError:
+    print("no such file exist my lord")
+except Exception:
+    print("no clues")
+    
+    try:
+        with open("tewst.txt",'r') as r:
+            print(r.read())
+    
+        
+  
+except FileNotFoundError as f:
+    print(f)
+except Exception as e:
+    print(e)
+else:
+    print("executing else")  #else block will run when there is no error in the try block  
+finally:
+    print("exection of finall block being done")  #this bock exectes no matter if there is an error or not
+
+
+
 
 try:
     f = open('curruptfile.txt')
